7628119f0d031df6b642ee2dcf2c0b767d023b93
Charvi-sys <charvimaggu@gmail.com>
2026-09-12 14:55:50 +0530

    Updates

--- assets/Resume-CHlgVvcG.js
+++ assets/Resume-CHlgVvcG.js
@@ -1,4 +1,4 @@
-import{s as Ju,r as Qu,j as he}from"./three-BJh4oU0n.js";import{P as et,m as Wa}from"./index-CZ9OUocz.js";import{S as _u}from"./skills-Brfcjkqz.js";import{P as $u}from"./PageShell-DFNuqyA9.js";import"./gsap-SFc2wnMY.js";/*! *****************************************************************************
+import{s as Ju,r as Qu,j as he}from"./three-BJh4oU0n.js";import{P as et,m as Wa}from"./index-BHlBFK6p.js";import{S as _u}from"./skills-Brfcjkqz.js";import{P as $u}from"./PageShell-CN1ruDJf.js";import"./gsap-SFc2wnMY.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
